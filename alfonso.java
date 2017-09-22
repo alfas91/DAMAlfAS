e27cb2086210fce8746358079d57d9f9a771f985
Alfonso <damalfassandres@gmail.com>
2017-09-22 13:28:04 +0200

    Muestra la localidad de nacimiento

--- alfonso.java
+++ alfonso.java
@@ -1,6 +1,6 @@
 public class alfonso {
 
 	public static void main(String args[]) {
-		System.out.println("Alfonso Alvarez Suarez, veintiuno.");
+		System.out.println("\n Alfonso Alvarez Suarez.\n Veintiuno.\n Leon.");
 	}
 }
