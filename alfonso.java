3330c96f8cabf1753d660a4efe383d1ea2208fc4
Alfonso <damalfassandres@gmail.com>
2017-09-22 13:16:49 +0200

    Muestra la edad

--- alfonso.java
+++ alfonso.java
@@ -1,6 +1,6 @@
 public class alfonso {
 
 	public static void main(String args[]) {
-		System.out.println("Alfonso Alvarez Suarez.");
+		System.out.println("Alfonso Alvarez Suarez, veintiuno.");
 	}
 }
